f1da563efd5e1065d301fc221ade92e8e2814373
Negoiu <negoiupaulica21@gmail.com>
2023-02-26 01:52:53 +0200

    Changed error handling
    It now throws TokenExpiredError.

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,23 +1,35 @@
-const jwt = require('jsonwebtoken');
+const { verify } = require('jsonwebtoken');
+const { TokenExpiredError } = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
-	const authHeader = req.headers.authorization || req.headers.Authorization;
-	if (!authHeader?.startsWith("Bearer ")) {
-		return res.sendStatus(401);
-	}
-	const token = authHeader.split(" ")[1];
-	console.log(token);
-	jwt.verify(
-		token,
-		process.env.ACCESS_TOKEN_SECRET,
-		(err, decoded) => {
-			if (err) {
-				return res.sendStatus(403);
-			}
-			req.user = decoded.email; // wtf is this for?
-			next();
+	try {
+		const authHeader = req.headers.authorization || req.headers.Authorization;
+		if (!authHeader?.startsWith("Bearer ")) {
+			throw new TokenExpiredError("Token not found");
 		}
-	);
+		const token = authHeader.split(" ")[1];
+		console.log(token);
+		verify(
+			token,
+			process.env.ACCESS_TOKEN_SECRET,
+			(err, decoded) => {
+				if (err) {
+					throw new TokenExpiredError("Token not found");
+				}
+				req.user = decoded.email; // wtf is this for?
+				next();
+			}
+		);
+	} catch (err) {
+		console.log(err);
+		let status;
+		if (err instanceof TokenExpiredError) {
+			status = 401;
+		} else {
+			status = 500;
+		}
+		res.status(status).send({ error: err.message });
+	}
 };
 
 module.exports = verifyJWT;
