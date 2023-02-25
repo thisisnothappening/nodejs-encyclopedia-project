804f86893242972c2648b77c0e85ca83aeb6227a
Negoiu <negoiupaulica21@gmail.com>
2023-02-26 01:51:15 +0200

    added wikipedia reference brackets regex
    1) Added wikipediaReferenceBracketsRemover
    
    2) Changed directory name from "data" to "docs"

--- config/articleReader.js
+++ config/articleReader.js
@@ -3,48 +3,58 @@ const { db, Article, Category } = require("../model/models.js");
 const fsPromises = require('fs').promises;
 const NullFieldError = require("../error/NullFieldError");
 const path = require("path");
-const InvalidFilePathError = require("../error/InvalidFilePathError.js");
 
-const reset = async (filePath) => {
-	if (typeof filePath !== "string") {
-		throw new InvalidFilePathError("filePath must be a string");
-	}
-	await db.sync({ force: true }) // this function truncates the tables
-		.catch(err => console.log(err));
+const fileArticlesPath = path.join(__dirname, "..", "docs", "articles.txt");
+const fileArticlesImprovedPath = path.join(__dirname, "..", "docs", "articles_improved.txt");
 
+const wikipediaReferenceBracketsRemover = async () => {
 	try {
-		const data = await fsPromises.readFile(filePath, "utf8");
+		await fsPromises.writeFile(fileArticlesImprovedPath, "");
+		const data = await fsPromises.readFile(fileArticlesPath, "utf-8");
 		let lines = data.split("\n");
 		for (let i = 0; i < lines.length; i++) {
 			let line = lines[i];
+			await fsPromises.appendFile(fileArticlesImprovedPath, line.replace(new RegExp("\\[(.*?)]", "g"), ""));
+			await fsPromises.appendFile(fileArticlesImprovedPath, "\n");
+		}
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+const reset = async () => {
+	await wikipediaReferenceBracketsRemover();
+	try {
+		await db.sync({ force: true }); // this function truncates the tables
+
+		const data = await fsPromises.readFile(fileArticlesImprovedPath, "utf-8");
+		let lines = data.split("\n");
+		for (let i = 0; i < lines.length; i++) {
+			let line = lines[i];
+			if (line.trim().length === 0) continue;
 			const elements = line.split(" | ");
 			const name = elements[0];
 			const category = elements[1];
 			const picture = elements[2];
 			const text = elements[3];
-			try {
-				if (!name || name.trim().length === 0 ||
-					!category || category.trim().length === 0 ||
-					!picture || picture.trim().length === 0 ||
-					!text || text.trim().length === 0) {
-					throw new NullFieldError("Field cannot be null");
-				}
-			} catch (err) {
-				return console.error(err);
+
+			if (!name || name.trim().length === 0 ||
+				!category || category.trim().length === 0 ||
+				!picture || picture.trim().length === 0 ||
+				!text || text.trim().length === 0) {
+				throw new NullFieldError("Field cannot be null");
 			}
 
 			const [categoryObject] = await Category.findOrCreate({
 				where: { name: category },
 				defaults: { name: category }
-			})
-				.catch(err => console.log(err));
+			});
 			await Article.create({
 				name: name,
 				categoryId: categoryObject.id,
 				picture: picture,
 				text: text,
-			})
-				.catch(err => console.log(err));
+			});
 		};
 		console.log("\n\t--- DATABASE HAS BEEN RESET ---");
 	} catch (err) {
@@ -52,4 +62,4 @@ const reset = async (filePath) => {
 	}
 };
 
-reset(path.join(__dirname, "..", "data", "articles.txt"));
+reset();
