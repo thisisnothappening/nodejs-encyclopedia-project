e1b781846ba99fbb119b40e8372df9003b3c2477
Negoiu <negoiupaulica21@gmail.com>
2023-04-10 00:22:01 +0300

    Automated NODE_ENV "dev": "cross-env NODE_ENV=development nodemon server.js", "prod": "cross-env NODE_ENV=production node server.js"
    Normally it's "start" instead of "prod", but I know many times I will type "start" by accident instead of "dev", so nah man.
    
    Also, added .env.development and .env.production stuff to database.js

--- config/database.js
+++ config/database.js
@@ -1,26 +1,24 @@
 const Sequelize = require("sequelize");
-require("dotenv").config();
+const dotenv = require("dotenv");
 
-let db;
+console.log(`\n\t--- Environment:   ${process.env.NODE_ENV} ---\n`)
 
-if (process.env.NODE_ENV === "development") {
-	db = new Sequelize("encyclopedia_test", "DBAdmin", "P120401", {
-		host: "localhost",
-		port: 3306,
-		dialect: "mysql",
-	});
+if (process.env.NODE_ENV === "development") 
+	dotenv.config({ path: ".env.development" });
+if (process.env.NODE_ENV === "production") 
+	dotenv.config({ path: ".env.production" });
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	dialect: process.env.DB_DIALECT,
+});
+
+if (process.env.NODE_ENV === "development")
 	db.sync({ alter: true, match: /_test$/ })
 		.catch(err => console.error(err));
-}
-
-else if (process.env.NODE_ENV === "production") {
-	db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
-		dialect: process.env.DB_DIALECT
-	});
+else
 	db.sync()
 		.catch(err => console.error(err));
-}
 
 module.exports = db;
