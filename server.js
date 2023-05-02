64dcdd0ad14ae701d09ef345352a231e3d60e09b
Negoiu <negoiupaulica21@gmail.com>
2023-05-02 23:59:47 +0300

    Added process.env.CORS_ORIGIN to be able to add more origins after the image was built.

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ db.authenticate()
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({
+	origin: [
+		"http://localhost:3000",
+		process.env.CORS_ORIGIN
+	],
+	credentials: true
+}));
 
 app.use("/users", require("./routes/users"));
 app.use(require("./routes/auth"));
