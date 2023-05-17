2ef59249531128845e6fe98e13206c05c871f89d
Negoiu <negoiupaulica21@gmail.com>
2023-05-17 23:26:27 +0300

    logError and logRequest now create /logs dir, if it doesnt't already exist.

--- middleware/logError.js
+++ middleware/logError.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
 const logError = (err) => {
 	let logMessage = `${new Date().toUTCString()} - ${err.name}: ${err.message}\n`;
+	const logsDir = path.join(__dirname, 'logs');
 
-	fs.appendFile('logs/error.log', logMessage, (err) => {
+	if (!fs.existsSync(logsDir)) {
+		fs.mkdirSync(logsDir);
+	}
+
+	fs.appendFile(path.join(logsDir, 'error.log'), logMessage, (err) => {
 		if (err) {
 			console.error('Error logging to file', err);
 		}
--- middleware/logRequest.js
+++ middleware/logRequest.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
 const logRequest = (message) => {
 	let logMessage = `${new Date().toUTCString()} - ${message}\n`;
+	const logsDir = path.join(__dirname, 'logs');
 
-	fs.appendFile('logs/request.log', logMessage, (err) => {
+	if (!fs.existsSync(logsDir)) {
+		fs.mkdirSync(logsDir);
+	}
+
+	fs.appendFile(path.join(logsDir, 'request.log'), logMessage, (err) => {
 		if (err) {
 			console.error('Error logging to file', err);
 		}
