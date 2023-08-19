eaaa036dd62ee63a2c5b0e8ea8703275e66b084d
Negoiu <negoiupaulica21@gmail.com>
2023-08-19 20:30:10 +0300

    docker-up.sh improved - added `buildx` to the build command, because I "need it" for some fucking reason - now you can also specify the extension of the Dockerfile in the command 	example: ./docker-up.sh nodejs-encyclopedia-project:latest .start

--- docker-up.sh
+++ docker-up.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Please provide the image name and tag in the format 'name:tag'."
+if [ "$#" -lt 1 ]; then
+    echo "Please provide the image name and tag in the format 'name:tag'. 
+Also, if your Dockerfile has an extension, such as Dockerfile.start, 
+then the format has to be 'name:tag .extension', 
+if not, then don't write anything after tag."
     exit 1
 fi
 
 IFS=':' read -ra IMAGE <<< "$1"
 IMAGE_NAME="${IMAGE[0]}"
 IMAGE_TAG="${IMAGE[1]}"
+DOCKERFILE_EXT=""
+
+if [ "$#" -gt 1 ]; then
+    DOCKERFILE_EXT="${@:2}"
+fi
 
 USERNAME="thisisnothappening"
+DOCKERFILE="Dockerfile${DOCKERFILE_EXT}"
 
-docker build -t "$IMAGE_NAME:$IMAGE_TAG" -f Dockerfile.start .
+docker buildx build -t "$IMAGE_NAME:$IMAGE_TAG" -f "$DOCKERFILE" .
 
 docker tag "$IMAGE_NAME:$IMAGE_TAG" "$USERNAME/$IMAGE_NAME:$IMAGE_TAG"
 
