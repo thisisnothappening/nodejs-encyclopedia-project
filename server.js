c65c00cceddf56eddd96afee614e3b8afe37f7bc
Negoiu <negoiupaulica21@gmail.com>
2023-08-19 21:17:06 +0300

    I turned this function into an arrow function It's just cooler, mmkay?

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(errorHandler);
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
